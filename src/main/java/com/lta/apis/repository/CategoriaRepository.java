77d574ecce460e4db590ab4a76c4291dcbf589a6
Luis Armuto <luisarmuto3@gmail.com>
2025-11-10 08:49:41 -0500

    feat(categoria): repositorio categoria

--- src/main/java/com/lta/apis/repository/CategoriaRepository.java
+++ src/main/java/com/lta/apis/repository/CategoriaRepository.java
@@ -1,4 +1,15 @@
 package com.lta.apis.repository;
 
-public interface CategoriaRepository {
+import com.lta.apis.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByNombreCategoria(String nombreCategoria);
+
+    boolean existsByNombreCategoria(String nombreCategoria);
+
 }
